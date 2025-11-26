40e80cbd75251fb0269222c551d8fb147bd8fac2
sathesh <satheeshkumar.palanisamy@ibridgellc.com>
2025-11-26 17:35:39 +0530

    Deploy code update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Set the base path for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
