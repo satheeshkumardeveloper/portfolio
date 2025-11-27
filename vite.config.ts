3b5453f1de314a25e2ce210f978b5a9ed9a63686
sathesh <satheeshkumar.palanisamy@ibridgellc.com>
2025-11-27 12:08:48 +0530

    Revert Last Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: '/', // Set the base path
+  base: command === 'build' ? '/portfolio/' : '/', // Set the base path conditionally
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 }));
+
