ea08c175c6180a4830ec346fb47e58f1130740b4
sathesh <satheeshkumar.palanisamy@ibridgellc.com>
2025-11-27 12:37:01 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/portfolio/' : '/', // Set the base path conditionally
+  base: '/portfolio/', // Set the base path conditionally
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
